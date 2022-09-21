8e66e3358387be456ddc310d96736b73096019d4
gedon55 <108569235+gedon55@users.noreply.github.com>
2022-09-21 21:15:51 +0000

    break

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,27 +1,27 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
-  
+
   def new
     @group = Group.new
   end
-  
+
   def index
     @post = Post.new
     @group = Group.all
   end
-  
+
   def show
     @post = Post.new
     @group = Group.find(params[:id])
   end
-  
+
   def join
     @group = Group.find(params[:group_id])
     @group.users << current_user
     redirect_to  groups_path
   end
-  
+
   def create
     @group = current_user.owned_groups.new(group_params)
     @group.owner_id = current_user.id
@@ -33,31 +33,48 @@ class GroupsController < ApplicationController
       render "index"
     end
   end
-  
+
   def edit
+    @group = Group.find(params[:id])
   end
-  
+
   def update
+    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to groups_path
     else
       render "edit"
     end
   end
-  
+
   def destroy
     @group = Group.find(params[:id])
     # current_userは、@group.usersから消される記述
     @group.users. delete(current_user)
     redirect_to groups_path
   end
-  
+
+  def all_destroy
+    @group = Group.find(params[:group_id])
+    if @group.destroy
+    redirect_to groups_path
+    end
+  end
+
+  def search
+    if params[:name].present?
+      @groups = Group.where('name LIKE ?', "%#{params[:name]}%")
+    else
+      @groups = Group.none
+    end
+  end
+
   private
-  
+
   def group_params
     params.require(:group).permit(:name, :introduction, :image)
   end
-  
+
   def ensure_correct_user
     @group = Group.find(params[:id])
     unless @group.owner_id == current_user.id
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root :to =>"homes#top"
   get 'homes/about'
-  
+
   resources :groups, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
+    get :search, on: :collection
     get "join" => "groups#join"
+      delete "all_destroy" => "groups#all_destroy"
   end
-  
-  resources :users, only: [:show, :edit, :update] 
+
+  resources :users, only: [:show, :edit, :update]
   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
       get :search, on: :collection
   end
-     
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
