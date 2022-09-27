df03a8d89b34a1d5d144e35f16a8497b5ced38f4
gedon55 <108569235+gedon55@users.noreply.github.com>
2022-09-27 09:03:01 +0000

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root :to =>"homes#top"
-  
+
   # ゲストログイン機能
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
@@ -12,15 +12,16 @@ Rails.application.routes.draw do
     get "join" => "groups#join"
       delete "all_destroy" => "groups#all_destroy"
   end
-  
+
   resources :group_users, only:[:update]
 
-  resources :users, only: [:show, :update] 
-  
+  resources :users, only: [:show, :update]
+
+  get "favorites" => "fovorites#index"
   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
-    resource :favorites, only: [:create, :destroy, :index]
+    resource :favorites, only: [:create, :destroy]
   end
-  
+
   resources :items, only: [:create, :index, :show, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
